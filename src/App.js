b9d4b292d8e01c3abb7c21670c5340241abfa636
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-04-08 19:39:06 +0000

    取得したデータから画像表示　--date=April 9 00:59:59 2023 +0900

--- src/App.js
+++ src/App.js
@@ -5,31 +5,31 @@ function App(){
   const[images,setImages] = useState([])
   const[text,setText] = useState("")
   const[query,setQuery] = useState('apple')
-  
+
   useEffect(() => {
     console.log('useEffect発火')
-    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=process.env.REACT_APP_CLIENT_ID`)
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
     .then(response => response.json())
     .then(data => {
       console.log(data)
       setImages(data.results)
     })
   }, [query] )
-  
+
   const onSubmit = (e) => {
     e.preventDefault();
     setQuery(text)
     setText('')
     console.log('onSubmit発火')
   }
-    
+
   return(
     <div className="App">
      <div className="main">
-     
+
       <form onSubmit = {onSubmit}>
-      
-       <input 
+
+       <input
           type="text"
           onChange={e => setText(e.target.value)}
           value = {text}
@@ -37,9 +37,21 @@ function App(){
         <button type="submit">
           search
         </button>
-        
-        
       </form>
+
+      <div className="container">
+       {
+       images.map(image => (
+         <div key={image.id} className="card">
+            <img src={image.urls.regular} className="card-img"/>
+            <div className="card-content">
+              <div className="card-title">{image.alt_discription}</div>
+            </div>
+         </div>
+       ))
+
+       }
+      </div>
      </div>
     </div>
     )
