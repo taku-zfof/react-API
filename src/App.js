f3ef98abb663df25fe6a6d670eeb68d669a6bcc7
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-04-08 19:06:28 +0000

    API実装

--- src/App.js
+++ src/App.js
@@ -2,17 +2,45 @@ import React, { useState,useEffect } from 'react'
 import './App.css';
 
 function App(){
-  const[count, setCount] = useState(0)
-
-   useEffect(()=>{
-    console.log(count)
-  },[count]
-    )
+  const[images,setImages] = useState([])
+  const[text,setText] = useState("")
+  const[query,setQuery] = useState('apple')
+  
+  useEffect(() => {
+    console.log('useEffect発火')
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=process.env.REACT_APP_CLIENT_ID`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setImages(data.results)
+    })
+  }, [query] )
+  
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setQuery(text)
+    setText('')
+    console.log('onSubmit発火')
+  }
     
   return(
-    <div>
-     <p>you clicked {count} times.</p>
-     <button onClick = {()=>setCount(count + 1) }> click me!</button>
+    <div className="App">
+     <div className="main">
+     
+      <form onSubmit = {onSubmit}>
+      
+       <input 
+          type="text"
+          onChange={e => setText(e.target.value)}
+          value = {text}
+        />
+        <button type="submit">
+          search
+        </button>
+        
+        
+      </form>
+     </div>
     </div>
     )
 }
