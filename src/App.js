e7dfd83a9a86b245d6bf8c547a88a93140779e22
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-04-08 17:08:55 +0000

    useState確認

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import './App.css';
 
 function App(){
   const[count, setCount] = useState(0)
+
+   useEffect(()=>{
+    console.log(count)
+  },[count]
+    )
+    
   return(
     <div>
      <p>you clicked {count} times.</p>
