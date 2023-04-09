705204e8df825d104eedc43606fce860f93c1922
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-04-09 12:00:00 +0900

    useEffectの確認

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react'
 import './App.css';
 
-
+function App(){
+  const[count, setCount] = useState(0)
+  return(
+    <div>
+     <p>you clicked {count} times.</p>
+     <button onClick = {()=>setCount(count + 1) }> click me!</button>
+    </div>
+    )
+}
 
 export default App;
